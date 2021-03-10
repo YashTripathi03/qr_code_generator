524007cae7c08589b48b6dee1ba16d1b343f4683
YashTripathi03 <80109678+YashTripathi03@users.noreply.github.com>
2021-03-10 08:10:05 +0530

    Add files via upload

---
+++ qr_code.py
@@ -0,0 +1,26 @@
+import os  #import os 
+import pyqrcode  #import pyqrcode
+from PIL import Image  #import image from PIL
+
+
+class QR_generator(object):  #define a class
+    """docstring for QR_generator"""
+
+    def __init__(self, text):
+        self.qr_image = self.qr_generator(text)
+
+    @staticmethod
+    def qr_generator(text):
+        qr_code = pyqrcode.create(text)
+        file_name = "QR Code Result"
+        save_path = os.path.join(os.path.expanduser('~'), 'Desktop')
+
+        name = f"{save_path}{file_name}.png"
+        qr_code.png(name, scale=10)
+        image = Image.open(name)
+        image = image.resize((400, 400), Image.ANTIALIAS)
+        image.show()
+
+
+if __name__ == '__main__':
+    QR_generator(input("[QR] Enter text or link:"))
